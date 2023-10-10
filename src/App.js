b7a2bc75f2840f1519f89ee1b84ed80a186a5c08
Nazisg <isgnezrin@gmail.com>
2023-10-10 19:03:22 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [random, setRandom] =useState(Math.floor(Math.random()*100))
+  const [inputValue,setInputValue]=useState('')
+  const [count, setCount] = useState(0)
+  const [showModal, setShowModal] = useState(false)
+
+  console.log(random)
+
+  const InputChange =(e)=>{
+    setInputValue(e.target.value)
+  }
+
+  const Give =()=>{
+    var randomRound = Math.round(random / 10) * 10
+    alert(`The number is near ${randomRound}s`)
+  }
+  
+  const Submit =()=>{
+    if(+inputValue!==random){
+      setCount(count+1)
+      console.log(false)
+    }else{
+      console.log(true)
+      console.log(count)
+    }
+    setShowModal(!showModal)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Guessing Game</h1>
+    <p>Enter a guess from 1-100. Good luck</p>
+    <input type="text" placeholder="Enter a guess" value={inputValue} onChange={InputChange}/>
+    <div className="btn-box">
+      <button className="btn give" onClick={Give}>Give a hint</button>
+      <button className="btn submit" onClick={Submit}>Submit</button>
+      <button className="btn start">Start over</button>
+    </div>
+    <div className='modal'>
+      {count} cehdde tapdiniz
+    </div>
     </div>
   );
 }
